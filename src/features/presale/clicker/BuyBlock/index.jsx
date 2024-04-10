facb5ac805a37f85914b220479ef3bb2d1bccc1e
Valeriy0 <v.kistrinov@yandex.ru>
2024-04-10 20:58:57 +0300

    fit

--- src/features/presale/clicker/BuyBlock/index.jsx
+++ src/features/presale/clicker/BuyBlock/index.jsx
@@ -89,15 +89,8 @@ export const BuyBlock = ({ checkNft, nftList, isAllowReflink }) => {
         let gas = null;
         const priceNft = toWei(fecthingNftList[currentNumCard]?.price);
 
-        console.log(fecthingNftList[currentNumCard]?.price);
-
-        // const arrayWithIds = id > 8 ? [9,10,11,12] : id > 4 ? [5,6,7,8] : [1,2,3,4];
-         const arrayWithIds = [Number(id)];
+        const arrayWithIds = [Number(id)];
         const arrayWithAmounts = [1];
-        // const gap = id > 8 ? id - 8 : id > 4 ? id - 4 : id;
-        // arrayWithIds.splice(gap - 1, 1, Number(id));
-        // arrayWithAmounts.splice(gap - 1, 1, 1);
-        console.log(arrayWithIds, arrayWithAmounts)
 
         try {
           gas = await contract.estimateGas.buy(refWallet, arrayWithIds, arrayWithAmounts, {
