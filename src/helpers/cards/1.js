289ade130b557c2debb79b66db19ec6f126a6011
Valeriy0 <v.kistrinov@yandex.ru>
2024-04-16 21:24:57 +0300

    update

--- src/helpers/cards/1.js
+++ src/helpers/cards/1.js
@@ -1,6 +1,5 @@
 export const nft1 = {
   "id": "1",
-  "price": 0.08,
   "description": "The set of suits for the Cougars from the MEO World p2e game. Each costume is a pass to MEO Interplanet Tournament with a prize pool in FRGX Tokens.",
   "image": "https://meo.world/MeoKit/Interplanet/img/pants_astronaut.png",
   "name": "Astronaut pants",
--- src/helpers/cards/2.js
+++ src/helpers/cards/2.js
@@ -1,6 +1,5 @@
 export const nft2 = {
   "id": "2",
-  "price": 0.04,
   "description": "The set of suits for the Cougars from the MEO World p2e game. Each costume is a pass to MEO Interplanet Tournament with a prize pool in FRGX Tokens.",
   "image": "https://meo.world/MeoKit/Interplanet/img/tail_astronaut.png",
   "name": "Astronaut tail",
